d8bef0824aa4aed6440aa483608f8be6c28ee3e5
Tony Million <tonymillion@gmail.com>
2011-06-26 10:33:46 +0100

    Stops view going screwey when the app is brought back to the foreground
    Stops view going screwey when the app is brought back to the foreground
    
    Uses a single UIImage for bubbles to save on memory
    
    scrolls tableview properly when the text entry box expands or contracts
    NOTE: there appears to be a bug in that it will always scroll to the bottom on first text entry because previousContentHeight is incorrect!

--- Application/Controllers/ChatViewController.h
+++ Application/Controllers/ChatViewController.h
@@ -4,11 +4,15 @@
 
 @interface ChatViewController : UIViewController <NSFetchedResultsControllerDelegate,
 UITableViewDelegate, UITableViewDataSource, UITextViewDelegate, UIActionSheetDelegate> {
+    
+    CGRect keyboardEndFrame;
+
 
 }
 
 @property (nonatomic, assign) SystemSoundID receiveMessageSound;
 
+@property (nonatomic, retain) UIView        *contentView;
 @property (nonatomic, retain) UITableView *chatContent;
 
 @property (nonatomic, retain) UIImageView *chatBar;
@@ -22,15 +26,18 @@ UITableViewDelegate, UITableViewDataSource, UITextViewDelegate, UIActionSheetDel
 @property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
 @property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
 
+@property (nonatomic, retain) UIImage       *clearballoon;
+@property (nonatomic, retain) UIImage       *greenballoon;
+
+@property (nonatomic, assign) BOOL          keyboardIsShowing;
+
 - (void)enableSendButton;
 - (void)disableSendButton;
 - (void)resetSendButton;
 
 - (void)keyboardWillShow:(NSNotification *)notification;
 - (void)keyboardWillHide:(NSNotification *)notification;
-- (void)slideFrameUp;
-- (void)slideFrameDown;
-- (void)slideFrame:(BOOL)up;
+- (void)slideFrame:(BOOL)up curve:(UIViewAnimationCurve)curve duration:(NSTimeInterval)duration;
 - (void)scrollToBottomAnimated:(BOOL)animated;
 
 - (void)sendMessage;
