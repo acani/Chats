6abe4062fd3449b3835aeaf2596e2d7cd024a183
Matt Di Pasquale <liveloveprosper@gmail.com>
2012-08-29 19:47:27 -0500

    Simplify Core Data stack setup.

--- AcaniChat/AppDelegate.h
+++ AcaniChat/AppDelegate.h
@@ -5,12 +5,4 @@
 @property (strong, nonatomic) UIWindow *window;
 @property (strong, nonatomic) UINavigationController *navigationController;
 
-@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
-@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
-- (void)saveContext;
-- (NSURL *)applicationDocumentsDirectory;
-
-
 @end
